55868817195d0fe176390f685b334ffaa7bdd268
blumkvist1 <101320457+blumkvist1@users.noreply.github.com>
2024-03-15 20:35:17 +0300

    Создание приложения .NET MAUI - заметки

---
+++ Lab3/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        }
+    }
+}
